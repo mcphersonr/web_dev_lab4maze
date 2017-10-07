4f83ec076b5039c80427022d87b6a10b735b37ca
Ramone McPherson <ramonemcpherson@gmail.com>
2017-10-07 21:53:48 +0000

    On-Page status update

--- maze.js
+++ maze.js
@@ -1,11 +1,12 @@
 window.onload= function(){
     let hitcount= 0;
     let startcount = 0;
+    let head = document.getElementById("status");
     let lose = document.getElementById("boundary1");
     lose.addEventListener("mouseover", function(){
         lose.classList.add("youlose");
         hitcount++;
-        console.log(hitcount);
+        head.innerHTML = "YOU LOSE!";
     });
     
     let otherlosses = document.querySelectorAll(".boundary");
@@ -13,26 +14,29 @@ window.onload= function(){
         otherlosses[i].addEventListener("mouseover",function(){
             otherlosses[i].classList.add("youlose");
             hitcount++;
-            console.log(hitcount);
+            head.innerHTML = "YOU LOSE!";
         });
     }
+    
+    let start = document.getElementById("start");
+    start.addEventListener("mouseover",function(){
+        startcount=1;
+    });
 
-    let restart = document.getElementById("start");
+    let restart = start;
     restart.addEventListener("click", function(){
         lose.classList.remove("youlose");
-        for(s=1; s<5; s++){
+        for(let s=1; s<5; s++){
             otherlosses[s].classList.remove("youlose");
         }
         hitcount = 0;
-        console.log(hitcount);
-        startcount = 1;
-        restart.innerHTML="S";
+        head.innerHTML="Move your mouse over the "+"S"+" to begin.";
     })
     
     let end = document.getElementById("end");
     end.addEventListener("mouseover", function (){
-        if(hitcount === 0){
-            alert("You win!");
+        if(hitcount === 0 && startcount ===1){
+           head.innerHTML = "YOU WIN!";
         }
     });
 }
