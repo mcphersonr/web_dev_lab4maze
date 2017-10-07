9095ef0a1fd2277bd223add1f91bb1b6b6e48828
Ramone McPherson <ramonemcpherson@gmail.com>
2017-10-07 19:54:42 +0000

    Changed the colour of all the walls to red

--- maze.js
+++ maze.js
@@ -1,16 +1,20 @@
 window.onload= function(){
     let lose = document.getElementById("boundary1");
-    lose.addEventListener("mouseover", mouseOver);
-    lose.addEventListener("mouseout", mouseOut);
-}
-
-
-function mouseOver(){
-    let lose = document.getElementById("boundary1");
-    lose.classList.add("youlose");
-}
-
-function mouseOut(){
-    let lose = document.getElementById("boundary1");
-    lose.classList.remove("youlose");
+    lose.addEventListener("mouseover", function(){
+        lose.classList.add("youlose");
+    });
+    lose.addEventListener("mouseout", function(){
+        lose.classList.remove("youlose");
+    });
+    
+    let other = document.querySelectorAll(".boundary");
+    console.log(other);    
+    for(let i=1; i<5; i++){
+        other[i].addEventListener("mouseover",function(){
+            other[i].classList.add("youlose");
+        });
+        other[i].addEventListener("mouseout",function(){
+          other[i].classList.add("youlose");
+        })
+    }
 }
